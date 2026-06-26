dbb022a5eee577d62ce55574817569e4a5771ba5
Krutarth Patel <mail2krutarthpatel@gmail.com>
2026-06-26 13:20:25 -0400

    sending emails every monday

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 import smtplib
+import datetime as dt
+import random
 
-myEmail = "xyz@gmail.com"
+quote = ""
+now = dt.datetime.now()
+if(now.weekday()==0):
+    with open("quotes.txt", "r") as quotesFile:
+        content = quotesFile.readlines()
+        quote = content[random.randint(0, len(content))]
+        
+    myEmail = "YOUR_EMAIL@gmail.com"
+    myPassword = "APP_PASSWORD"
+    with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+        connection.starttls()
+        connection.login(myEmail, myPassword)
+        connection.sendmail(from_addr=myEmail, to_addrs="RECIEVER_EMAIL@gmail.com", msg=f"Subject: Quote of the Week! \n\n{quote}")
 
-connection = smtplib.SMTP("smtp.gmail.com")
-connection.starttls()
-
+print("Email sent!")
